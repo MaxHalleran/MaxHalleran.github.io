4fdb5f5128e110170c616b4a291aaee2d9d2527e
MaxHalleran <maxhalleran@gmail.com>
2022-04-27 21:00:16 -0600

    Fixed layout issue with breakpoints importing after styles and updated copy content

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,10 +1,9 @@
-const gulp = require('gulp');
+const { series, watch, parallel } = require('gulp');
 
 const compileScripts = require("./scripts");
 const compileSass = require("./sass");
 const compileHTML = require("./html");
 const migrateDocs = require("./assets");
-const { series } = require('gulp');
 
 function defaultTask(cb) {
 	console.log("This is the default task for the personal portfolio.");
@@ -18,13 +17,24 @@ exports.html = compileHTML;
 exports.docs = migrateDocs;
 exports.scripts = compileScripts;
 
-function buildFull(cb) {
-	console.log("Building entire site");
-
-	console.log(compileSass);
-
-	series(compileSass, compileHTML, compileScripts, migrateDocs);
-	cb();
-}
-
 exports.buildFull = series(compileSass, compileHTML, compileScripts, migrateDocs);
+
+exports.watchHtml = () => {
+	console.log("Watching HTML");
+
+	return watch('./src/*.html', { ignoreInitial: false }, compileHTML);
+};
+
+exports.watchJs = () => {
+	console.log("Watching JS");
+
+	return watch('./src/scripts/*.js', { ignoreInitial: false }, compileScripts);
+};
+
+exports.watchSass = () => {
+	console.log("Watching CSS");
+
+	return watch('./src/**/*.scss', { ignoreInitial: false }, compileSass);
+};
+
+exports.watch = parallel( this.watchHtml, this.watchJs, this.watchSass );
